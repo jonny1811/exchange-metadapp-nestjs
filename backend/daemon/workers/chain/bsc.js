3155ffb9c294a4c5fdf9593810d3426b8a229b05
Jonny Lezcano <jlezcanoa@ext.falabella.cl>
2023-02-07 11:02:22 -0300

    feat: add queue to get info from blockchain

---
+++ backend/daemon/workers/chain/bsc.js
@@ -0,0 +1,16 @@
+const {
+	Worker,
+	connectDB,
+	createTransaction,
+	processDeposit
+} = require('./index')
+
+connectDB.then(() => {
+	new Worker('bnb-transactions', async (job) => {
+        return await createTransaction(job.data)
+    })
+
+    new Worker('bnb-deposits', async (job) => {
+        return await processDeposit(job.data)
+    })
+})
